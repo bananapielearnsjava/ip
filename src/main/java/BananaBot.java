876e976b311505658f163588df5af1c433d67685
Lew Si Yu <156303901+bananapielearnsjava@users.noreply.github.com>
2025-08-22 16:42:52 +0800

    Add mark and unmark task with Task Class

--- src/main/java/BananaBot.java
+++ src/main/java/BananaBot.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BananaBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> toDoList = new ArrayList<>();
+        List<Task> toDoList = new ArrayList<>();
 
         String logo = "                  __,__\n" +
                 "        .--.  .-\"     \"-.  .--.\n" +
@@ -37,23 +37,68 @@ public class BananaBot {
         System.out.println("Hello I'm BananaBot\n" + logo + "\nWhat can I do for you?" );
         while(true) {
             String input = scanner.nextLine();
-
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < toDoList.size(); i++) {
-                    System.out.println((i + 1) + ". " + toDoList.get(i));
+                    System.out.println((i + 1) + "." + toDoList.get(i).toString());
                 }
-            }
+                System.out.println("____________________________________________________________");
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5).trim()) - 1;
+                if (taskNumber >= 0 && taskNumber < toDoList.size()) {
+                    Task task = toDoList.get(taskNumber);
+                    if (!task.isDone()) {
+                        task.markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  [X] " + task.getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("This task is already marked as done!");
+                        System.out.println("____________________________________________________________");
+                    }
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Invalid task number!");
+                    System.out.println("____________________________________________________________");
+                }
 
-            else if (input.equalsIgnoreCase("bye")) {
+            } else if (input.toLowerCase().startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (taskNumber >= 0 && taskNumber < toDoList.size()) {
+                    Task task = toDoList.get(taskNumber);
+                    if (task.isDone()) {
+                        task.markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  [ ] " + task.getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("This task is already not done!");
+                        System.out.println("____________________________________________________________");
+                    }
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Invalid task number!");
+                    System.out.println("____________________________________________________________");
+                }
+
+            }else if (input.equalsIgnoreCase("bye")) {
                 System.out.println("\nBye. Hope to see you again soon!");
                 break;
-            }
 
-            else {
-                System.out.println("Added: " + input);
-                toDoList.add(input);
+            }else {
+                Task task = new Task(input);
+                toDoList.add(task);
+                System.out.println("____________________________________________________________");
+                System.out.println("Added: " + task);
+                System.out.println("____________________________________________________________");
             }
         }
+
+        scanner.close();
     }
 }
