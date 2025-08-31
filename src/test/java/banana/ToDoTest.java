490b569507a169ac5f188265f8b9a2458fe91acf
Lew Si Yu <156303901+bananapielearnsjava@users.noreply.github.com>
2025-08-31 20:51:32 +0800

    Add JUnit testcases for Deadline and Todo

---
+++ src/test/java/banana/ToDoTest.java
@@ -0,0 +1,25 @@
+package banana;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+
+    @Test
+    public void testToStringWithEmptyDescription() {
+        // Test with an empty description
+        ToDo toDo = new ToDo("");
+        String expectedOutput = "[T][ ] ";
+        assertEquals(expectedOutput, toDo.toString(), "The toString should handle empty description");
+    }
+}
